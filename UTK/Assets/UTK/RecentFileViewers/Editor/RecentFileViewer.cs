5125de049f37ed58600fa160094b57047daa452d
TeraiKatsuki <chibi201028@gmail.com>
2019-03-25 01:36:35 +0900

    Initial [RecentFileViewers]

---
+++ UTK/Assets/UTK/RecentFileViewers/Editor/RecentFileViewer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UTK.RecentFileViewer
+{
+    public class RecentFileViewer : EditorWindow
+    {
+        static RecentFileViewer recentFileViewer;
+
+        [SerializeField]
+        RecentFileViewerConfig config;
+
+
+        [MenuItem("UTK/RecentFileViewer",false,12)]
+        static void Open()
+        {
+            if (recentFileViewer == null)
+            {
+                recentFileViewer = CreateInstance<RecentFileViewer>();
+            }
+
+            recentFileViewer.config = RecentFileViewerConfig.GetRecentFileViewerConfig();
+
+            recentFileViewer.titleContent.text = "RecentFileViewer";
+            recentFileViewer.Show();
+        }
+
+        #region Internal
+
+        void OnGUI()
+        {
+            
+        }
+
+        #endregion
+    }
+
+}
---
+++ UTK/Assets/UTK/RecentFileViewers/Editor/RecentFileViewerConfig.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace UTK.RecentFileViewer
+{
+    public class RecentFileViewerConfig : ScriptableObject
+    {
+        public static readonly string CONFIGDIRECTORYPATH = "Assets/UTK/Config";
+        public static readonly string CONFIGFILEPATH = "Assets/UTK/Config/RecentFileViewerConfig.asset";
+
+        public static RecentFileViewerConfig GetRecentFileViewerConfig()
+        {
+            var config =
+                    (RecentFileViewerConfig)AssetDatabase.FindAssets("t:ScriptableObject", new string[] { CONFIGDIRECTORYPATH })
+                    .Select(id => AssetDatabase.GUIDToAssetPath(id))
+                    .Select(path => AssetDatabase.LoadAssetAtPath(path, typeof(RecentFileViewerConfig)))
+                    .Where(c => c != null)
+                    .FirstOrDefault();
+
+            if (config == null)
+            {
+                config = CreateInstance<RecentFileViewerConfig>();
+                Save(config);
+            }
+
+            return config;
+        }
+
+        private static void Save(RecentFileViewerConfig config)
+        {
+            if (!Directory.Exists(CONFIGDIRECTORYPATH))
+            {
+                Directory.CreateDirectory(CONFIGDIRECTORYPATH);
+            }
+
+            if (!File.Exists(CONFIGFILEPATH))
+            {
+                AssetDatabase.CreateAsset(config, CONFIGFILEPATH);
+                AssetDatabase.Refresh();
+            }
+
+        }
+    }
+}
+
