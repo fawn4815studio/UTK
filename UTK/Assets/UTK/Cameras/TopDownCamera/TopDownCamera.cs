a4ef10de9535b01da3ca3960b9384ef23adfd4b7
k-terai <chibi201028@gmail.com>
2019-05-12 23:44:25 +0900

    Update [TopDownCamera]
    ・ Add basic tracking function
    ・ Update the test scene

--- UTK/Assets/UTK/Cameras/TopDownCamera/TopDownCamera.cs
+++ UTK/Assets/UTK/Cameras/TopDownCamera/TopDownCamera.cs
@@ -2,17 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TopDownCamera : MonoBehaviour
+namespace UTK.Cameras
 {
-    // Start is called before the first frame update
-    void Start()
+    public class TopDownCamera : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        GameObject target;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField]
+        Vector3 offset;
+
+        [SerializeField]
+        [Range(0,5)]
+        float smoothTime;
+
+        [SerializeField]
+        bool enableLookAt;
+
+        Vector3 velocity = Vector3.zero;
+
+        #region Property
+        public GameObject Target { get => target; private set => target = value; }
+        public Vector3 Offset { get => offset; set => offset = value; }
+        public Vector3 Velocity { get => velocity;}
+        public bool EnableLookAt { get => enableLookAt; set => enableLookAt = value; }
+        #endregion
+
+        public void ChangeTarget(GameObject t)
+        {
+            target = t;
+        }
+
+        void LateUpdate()
+        {
+            if (target == null) return;
+
+            var newpos = target.transform.position + offset;
+            transform.position = Vector3.SmoothDamp(transform.position,newpos,ref velocity, smoothTime);
+
+            if(EnableLookAt)
+            {
+                transform.LookAt(target.transform);
+            }
+        }
+
     }
 }
+
