b938f15370d3f4c18b9cdcb0cc1f194da880f9c6
k-terai <chibi201028@gmail.com>
2019-05-14 02:05:48 +0900

    Initial [AutoStageBuilder]

---
+++ UTK/Assets/UTK/AutoStageBuilders/Editor/AutoStageBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace UTK.AutoStageBuilder
+{
+    public class AutoStageBuilder : EditorWindow
+    {
+        static AutoStageBuilder autoStageBuilder;
+
+        [MenuItem("UTK/Procedural/AutoStageBuilder", false, 2)]
+        static void Open()
+        {
+            if (autoStageBuilder == null)
+            {
+               autoStageBuilder = CreateInstance<AutoStageBuilder>();
+            }
+
+            autoStageBuilder.minSize = new Vector2(500, 400);
+            autoStageBuilder.titleContent.text = "AutoStageBuilder";
+            autoStageBuilder.ShowUtility();
+        }
+    }
+
+}
