ad12dd36ccdd046d8ea607a82f18c872070bd5f2
k-terai <chibi201028@gmail.com>
2019-06-30 00:01:09 +0900

    Initial ResourceManager

---
+++ UTK/Assets/UTK/Runtimes/Managers/Scripts/ResourceManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UTK.Runtimes.Managers
+{
+    public class ResourceManager : MonoBehaviour
+    {
+        #region Singleton
+
+        private static ResourceManager instance;
+        private ResourceManager() { }
+
+        public static ResourceManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    var ob = new GameObject("ResourceManager");
+                    instance = ob.AddComponent<ResourceManager>();
+
+                    if (Application.isPlaying)
+                    {
+                        DontDestroyOnLoad(ob);
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Start is called before the first frame update.
+        /// </summary>
+        void Start()
+        {
+
+        }
+
+        /// <summary>
+        /// Update is called once per frame.
+        /// </summary>
+        void Update()
+        {
+
+        }
+    }
+}
+
