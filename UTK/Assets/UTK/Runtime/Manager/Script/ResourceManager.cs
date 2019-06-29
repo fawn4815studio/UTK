d141a7db101bea4d26e5d2f690702b642080c44a
k-terai <chibi201028@gmail.com>
2019-06-30 02:00:56 +0900

    [ResourceManager] Add basic load and unload func

--- UTK/Assets/UTK/Runtime/Manager/Script/ResourceManager.cs
+++ UTK/Assets/UTK/Runtime/Manager/Script/ResourceManager.cs
@@ -1,11 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace UTK.Runtime.Manager
 {
+    /// <summary>
+    /// ResourceManager can not load AssetBundle. Use AssetBundleManager to load AssetBundle.
+    /// </summary>
     public class ResourceManager : MonoBehaviour
     {
+        private Dictionary<string, UnityEngine.Object> cacheDic = new Dictionary<string, UnityEngine.Object>();
+
         #region Singleton
 
         private static ResourceManager instance;
@@ -33,20 +39,91 @@ namespace UTK.Runtime.Manager
         #endregion
 
         /// <summary>
-        /// Start is called before the first frame update.
+        /// Load resource specified by path synchronously.
         /// </summary>
+        /// <param name="filepath">Resource file path.</param>
+        public Type LoadSync<Type>(string filepath)
+            where Type : UnityEngine.Object
+
+        {
+            if (cacheDic.ContainsKey(filepath))
+            {
+                return cacheDic[filepath] as Type;
+            }
+
+            var resource = Resources.Load<Type>(filepath);
+            cacheDic[filepath] = resource;
+
+            return resource;
+        }
+
+        /// <summary>
+        /// Loads the resource specified by path asynchronously.
+        /// </summary>
+        /// <param name="filepath">Resource file path.</param>
+        /// <param name="oncomplete">Called when resource loaded.</param>
+        public void LoadAsync<Type>(string filepath, UnityEngine.Events.UnityAction<Type> oncomplete)
+            where Type : UnityEngine.Object
+
+        {
+            if (cacheDic.ContainsKey(filepath))
+            {
+                oncomplete?.Invoke(cacheDic[filepath] as Type);
+            }
+
+            StartCoroutine(LoadAsyncCo(filepath, oncomplete));
+        }
+
+        /// <summary>
+        /// Unload resource if loaded.
+        /// </summary>
+        /// <param name="filepath">Unload resource file path.</param>
+        public void Unload(string filepath)
+        {
+            if (cacheDic.ContainsKey(filepath))
+            {
+                Resources.UnloadAsset(cacheDic[filepath]);
+                cacheDic.Remove(filepath);
+            }
+        }
+
+        /// <summary>
+        /// Unload all resource.
+        /// </summary>
+        public void UnloadAll()
+        {
+            cacheDic.Clear();
+            Resources.UnloadUnusedAssets();
+        }
+
+        #region Internal
+
+        IEnumerator LoadAsyncCo<Type>(string filepath, UnityEngine.Events.UnityAction<Type> oncomplete)
+              where Type : UnityEngine.Object
+        {
+            ResourceRequest resReq = Resources.LoadAsync(filepath);
+
+            while (resReq.isDone == false)
+            {
+                yield return null;
+            }
+
+            cacheDic[filepath] = resReq.asset;
+            oncomplete?.Invoke(resReq.asset as Type);
+        }
+
         void Start()
         {
 
         }
 
-        /// <summary>
-        /// Update is called once per frame.
-        /// </summary>
         void Update()
         {
 
         }
+
+        #endregion
+
     }
 }
 
---
+++ UTK/Assets/UTK/Runtime/Manager/Test/Script/ResourceTester.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UTK.Runtime.Manager.Test
+{
+    public class ResourceTester : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var resource = ResourceManager.Instance.LoadSync<GameObject>("CapsuleTestResource");
+            var ob = Instantiate(resource, Vector3.zero, Quaternion.identity);
+
+            ResourceManager.Instance.LoadAsync<GameObject>("CubeTestResource", (GameObject prefab) =>
+             {
+                 Instantiate(prefab, new Vector3(0, 5, 0), Quaternion.identity);
+             }
+            );
+
+            ResourceManager.Instance.LoadAsync<Material>("TestResourceMat", (Material mat) =>
+            {
+                ob.GetComponent<Renderer>().material = mat;
+            }
+            );
+
+            ResourceManager.Instance.UnloadAll();
+        }
+    }
+
+}
