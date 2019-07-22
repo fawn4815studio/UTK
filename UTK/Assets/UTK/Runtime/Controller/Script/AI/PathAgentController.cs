2babaebe7319a5b9d6a7242616286938c441adce
k-terai <chibi201028@gmail.com>
2019-07-23 00:33:42 +0900

    Initial [PathAgentController]

---
+++ UTK/Assets/UTK/Runtime/Controller/Script/AI/PathAgentController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UTK.Runtime.Controller.AI
+{
+    /// <summary>
+    /// AI controller with the ability to move along a specified path.
+    /// </summary>
+    public class PathAgentController : MonoBehaviour
+    {
+        /// <summary>
+        ///  Start is called before the first frame update
+        /// </summary>
+        void Start()
+        {
+
+        }
+
+        /// <summary>
+        /// Update is called once per frame
+        /// </summary>
+        void Update()
+        {
+
+        }
+    }
+
+}
---
+++ UTK/Assets/UTK/Runtime/Controller/Script/Simple/Editor/PathAgentControllerInspector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace UTK.Runtime.Controller.AI
+{
+    [CustomEditor(typeof(PathAgentController))]
+    public class PathAgentControllerInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
+    }
+
+}
