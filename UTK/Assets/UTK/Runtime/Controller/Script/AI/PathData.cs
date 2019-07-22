2b326fac20b15f467c1a79769e739c7dbde7d74c
k-terai <chibi201028@gmail.com>
2019-07-23 01:24:17 +0900

    Add  [PathData]

---
+++ UTK/Assets/UTK/Runtime/Controller/Script/AI/PathData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UTK.Runtime.Controller.AI
+{
+    /// <summary>
+    /// Path data used by Agent.
+    /// Use through <see cref="PathAgentController"/>.
+    /// </summary>
+    public class PathData
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private Vector3 position;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private GameObject target = null;
+
+        #region Property
+
+        public Vector3 Position { get => position; }
+
+        public GameObject Target { get => target; }
+
+        #endregion
+
+
+    }
+
+}
