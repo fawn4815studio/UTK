c70d3d4cce60ccb8501cca5015efc6c98cc0b6e6
k-terai <chibi201028@gmail.com>
2019-09-15 03:11:33 +0900

    (Runtime)(AudioManager) Implement the function to control BGM

---
+++ UTK/Assets/UTK/Runtime/AudioManager/Script/AudioData.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UTK.Runtime
+{
+    /// <summary>
+    /// Ausio management data.
+    /// Use through <see cref="Manager.AudioManager"/>.
+    /// </summary>
+    public class AudioData
+    {
+        #region Property
+
+        public string FilePath { get; private set; }
+
+        public string Name { get; private set; }
+
+        public AudioSource Owner { get; set; }
+
+        public AudioClip Clip { get; private set; }
+
+        public bool IsLoop { get; set; } = false;
+
+        public float Volume { get; set; } = 1.0f;
+
+        public float VolumeScale { get; set; } = 1.0f;
+
+        public bool Mute { get; set; } = false;
+
+        public bool IsPlaying
+        {
+            get
+            {
+                if (Owner == null) return false;
+                return Owner.isPlaying;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="name">Audio data name.</param>
+        /// <param name=""><see cref="AudioClip"/> file path.</param>
+        /// <param name="clip"><see cref="AudioClip"/> instance.</param>
+        public AudioData(string name, string filepath, AudioClip clip)
+        {
+            FilePath = filepath;
+            Name = name;
+            Clip = clip;
+        }
+
+    }
+}
+
--- UTK/Assets/UTK/Runtime/AudioManager/Script/AudioManager.cs
+++ UTK/Assets/UTK/Runtime/AudioManager/Script/AudioManager.cs
@@ -5,16 +5,124 @@ using UnityEngine;
 namespace UTK.Runtime.Manager
 {
     /// <summary>
-    /// Manager class for managing audio loading, playback, management, etc.
+    /// Manager class for managing audio loading, playback, etc.
     /// </summary>
     public class AudioManager : SingletonBase<AudioManager>
     {
+        private AudioSource bgmSource;
+        private Dictionary<string, AudioData> dataDic = new Dictionary<string, AudioData>();
+
+        /// <summary>
+        /// Load audio data specified by path synchronously.
+        /// </summary>
+        /// <param name="name">Register audio data name.</param>
+        /// <param name="filepath"><see cref="AudioClip"/> file path.</param>
+        /// <param name="loop">Is loop.</param>
+        /// <param name="volume">Audio volume.</param>
+        public void LoadSync(string name, string filepath, bool loop = false, float volume = 1.0f)
+        {
+            if (!dataDic.ContainsKey(name))
+            {
+                dataDic[name] = new AudioData(name, filepath, ResourceManager.Instance.LoadSync<AudioClip>(filepath))
+                {
+                    IsLoop = loop,
+                    Volume = volume
+                };
+            }
+            else
+            {
+                Debug.LogWarning(string.Format("{0} already exists.", name));
+            }
+        }
+
+        /// <summary>
+        /// Load audio data specified by path asynchronously.
+        /// </summary>
+        /// <param name="name">Register audio data name.</param>
+        /// <param name="filepath"><see cref="AudioClip"/> file path.</param>
+        /// <param name="loop">Is loop.</param>
+        /// <param name="volume">Audio volume.</param>
+        public void LoadAsync(string name, string filepath, bool loop = false, float volume = 1.0f)
+        {
+            if (!dataDic.ContainsKey(name))
+            {
+                ResourceManager.Instance.LoadAsync(filepath, (AudioClip clip) =>
+                {
+                    dataDic[name] = new AudioData(name, filepath, clip)
+                    {
+                        IsLoop = loop,
+                        Volume = volume
+                    };
+                });
+
+            }
+            else
+            {
+                Debug.LogWarning(string.Format("{0} already exists.", name));
+            }
+        }
+
+        /// <summary>
+        /// Unload audio data.
+        /// </summary>
+        /// <param name="name">Delete audio data name.</param>
+        public void Unload(string name)
+        {
+            if (dataDic.ContainsKey(name))
+            {
+                ResourceManager.Instance.Unload(dataDic[name].Clip.name);
+                dataDic.Remove(name);
+            }
+        }
+
+        /// <summary>
+        /// Play bgm.
+        /// </summary>
+        /// <param name="name">Bgm name.</param>
+        /// <param name="reset">True = Reset audio time.</param>
+        public void PlayBgm(string name, bool reset = false)
+        {
+            if (!dataDic.ContainsKey(name))
+            {
+                Debug.LogWarning(string.Format("Failed play bgm. {0} not loaded.", name));
+                return;
+            }
+
+            if (bgmSource == null)
+            {
+                bgmSource = gameObject.AddComponent<AudioSource>();
+            }
+
+            var data = dataDic[name];
+            bgmSource.clip = data.Clip;
+            bgmSource.loop = data.IsLoop;
+            bgmSource.volume = data.Volume;
+            bgmSource.mute = data.Mute;
+            if (reset) bgmSource.time = 0.0f;
+            bgmSource.Play();
+        }
+
+        /// <summary>
+        /// Stop bgm.
+        /// </summary>
+        public void StopBgm()
+        {
+            if (bgmSource.isPlaying)
+            {
+                bgmSource.Stop();
+            }
+        }
 
         #region Internal
 
         void Start()
         {
             name = "AudioManager";
+
+            if (bgmSource == null)
+            {
+                bgmSource = gameObject.AddComponent<AudioSource>();
+            }
         }
 
         void Update()
