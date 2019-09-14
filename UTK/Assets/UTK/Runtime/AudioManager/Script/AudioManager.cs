5352a6beb306c31ea1a7f2e8bfa504bc80378423
k-terai <chibi201028@gmail.com>
2019-09-14 19:36:52 +0900

    (Runtime)(AudioManager) Initial AudioManager

---
+++ UTK/Assets/UTK/Runtime/AudioManager/Script/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UTK.Runtime.Manager
+{
+    /// <summary>
+    /// Manager class for managing audio loading, playback, management, etc.
+    /// </summary>
+    public class AudioManager : SingletonBase<AudioManager>
+    {
+
+        #region Internal
+
+        void Start()
+        {
+            name = "AudioManager";
+        }
+
+        void Update()
+        {
+
+        }
+
+        #endregion
+    }
+
+}
