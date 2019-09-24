e0f81a1155123d870f01fc37d2def3a1daccc38f
k-terai <chibi201028@gmail.com>
2019-09-25 02:11:15 +0900

    (Runtime)(AutoDestroy) Add auto destroy class and test scene

---
+++ UTK/Assets/UTK/Runtime/Utility/AutoDestroy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace UTK.Runtime.Utility
+{
+    /// <summary>
+    /// Class that has a function to automatically delete object.
+    /// </summary>
+    public class AutoDestroy : MonoBehaviour
+    {
+        [SerializeField]
+        private float waitTime = 1.0f;
+
+        void Start()
+        {
+            StartCoroutine(DestroyCo());
+        }
+
+        IEnumerator DestroyCo()
+        {
+            yield return new WaitForSeconds(waitTime);
+            Destroy(gameObject);
+        }
+    }
+}
+
