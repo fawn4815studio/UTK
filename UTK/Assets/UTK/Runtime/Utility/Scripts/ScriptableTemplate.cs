1ede08b7f13fe86dadbbf01fbb7d01a6e9804dbb
k-terai <chibi201028@gmail.com>
2020-05-05 18:52:48 +0900

    (Runtime)(Utility) Add [ScriptableTemplate]

---
+++ UTK/Assets/UTK/Runtime/Utility/Scripts/ScriptableTemplate.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace UTK.Runtime.Utility
+{
+    public class ScriptableTemplate : ScriptableObject
+    {
+        /// <summary>
+        /// Change to a suitable storage location.
+        /// </summary>
+        private static readonly string SAVE_PATH = "Assets/UTK/Config/ScriptableTemplate.asset";
+        private static ScriptableTemplate instance;
+
+#if UNITY_EDITOR
+        [MenuItem("UTK/Utility/Open ScriptableTemplate")]
+        private static void CreateOrOpenScriptableTemplate()
+        {
+            Selection.activeObject = Instance;
+        }
+
+#endif
+
+        public static ScriptableTemplate Instance
+        {
+            get
+            {
+#if UNITY_EDITOR
+                var path = AssetDatabase.AssetPathToGUID(SAVE_PATH);
+                if (string.IsNullOrEmpty(path))
+                {
+                    instance = ScriptableObject.CreateInstance<ScriptableTemplate>();
+                    AssetDatabase.CreateAsset(instance, SAVE_PATH);
+                }
+#endif
+                return instance;
+            }
+        }
+
+
+    }
+
+}
