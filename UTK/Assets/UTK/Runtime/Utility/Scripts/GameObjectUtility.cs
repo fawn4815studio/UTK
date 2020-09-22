c83a5e42c6ecca381ff4eb6a502706881226622d
k-terai <chibi201028@gmail.com>
2020-09-23 01:26:22 +0900

    (Runtime)(Utility) Add [GameObjectUtility]

---
+++ UTK/Assets/UTK/Runtime/Utility/Scripts/GameObjectUtility.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace UTK.Runtime.Utility
+{
+    public static class GameObjectUtility
+    {
+        public static void SetActiveIfNeeded(this GameObject go,bool value)
+        {
+            if(go.activeSelf != value)
+            {
+                go.SetActive(value);
+            }
+        }
+    }
+
+}
